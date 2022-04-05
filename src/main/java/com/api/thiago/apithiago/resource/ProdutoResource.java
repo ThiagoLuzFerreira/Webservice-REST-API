155d35a35513e40029f8e5f90dcf7d134393dbf3
ThiagoLuzFerreira <97471784+ThiagoLuzFerreira@users.noreply.github.com>
2022-04-05 14:16:28 -0300

    Add files via upload

--- src/main/java/com/api/thiago/apithiago/resource/ProdutoResource.java
+++ src/main/java/com/api/thiago/apithiago/resource/ProdutoResource.java
@@ -1,7 +1,9 @@
 package com.api.thiago.apithiago.resource;
 
 import com.api.thiago.apithiago.domain.Produto;
-import com.api.thiago.apithiago.repositoty.ProdutoRepository;
+import com.api.thiago.apithiago.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,28 +12,34 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/apithiago")
+@Api(value="API REST Produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoResource {
 
     @Autowired
     private ProdutoRepository repo;
 
     @PostMapping("/produtos")
+    @ApiOperation(value="Salva um produto")
     @ResponseStatus(HttpStatus.CREATED)
     public Produto salvar(@RequestBody Produto produto){
         return repo.save(produto);
     }
 
     @GetMapping("/produtos")
+    @ApiOperation(value="Retorna lista de produtos")
     public List<Produto> listar(){
         return repo.findAll();
     }
 
     @GetMapping("/produtos/{id}")
+    @ApiOperation(value="Busca um produto único")
     public Produto listarPorId(@PathVariable("id") long id){
         return repo.findById(id);
     }
 
     @DeleteMapping("/produtos/{id}")
+    @ApiOperation(value="Deleta um produto")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable("id") long id, Produto produto){
         produto = repo.findById(id);
@@ -39,6 +47,7 @@ public class ProdutoResource {
     }
 
     @PutMapping("/produtos")
+    @ApiOperation(value="Edita um produto")
     public Produto editar(@RequestBody Produto produto){
         return repo.save(produto);
     }
