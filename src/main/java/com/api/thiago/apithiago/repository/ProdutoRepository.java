4e3f7c3d74f947b7b32632e45e8272177ad57d11
ThiagoLuzFerreira <97471784+ThiagoLuzFerreira@users.noreply.github.com>
2022-04-22 17:53:28 -0300

    Add files via upload

--- src/main/java/com/api/thiago/apithiago/repository/ProdutoRepository.java
+++ src/main/java/com/api/thiago/apithiago/repository/ProdutoRepository.java
@@ -1,11 +1,17 @@
 package com.api.thiago.apithiago.repository;
 
+import com.api.thiago.apithiago.domain.Faturado;
 import com.api.thiago.apithiago.domain.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     Produto findById(long id);
 
+    @Modifying
+    @Query("update Produto p set p.nome = ?1, p.faturado = ?2, p.quantidade = ?3, p.valor = ?4 where p.id = ?5")
+    void update(String nome, Faturado faturado, Integer quantidade, Float valor, long id);
 }
