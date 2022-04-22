bff9ff97f4625784aa60c00a43294a1b81f5dda9
ThiagoLuzFerreira <97471784+ThiagoLuzFerreira@users.noreply.github.com>
2022-04-22 11:15:11 -0300

    Add files via upload

--- src/main/java/com/api/thiago/apithiago/resource/ProdutoResource.java
+++ src/main/java/com/api/thiago/apithiago/resource/ProdutoResource.java
@@ -2,6 +2,8 @@ package com.api.thiago.apithiago.resource;
 
 import com.api.thiago.apithiago.domain.Produto;
 import com.api.thiago.apithiago.repository.ProdutoRepository;
+import com.api.thiago.apithiago.service.ProdutoService;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class ProdutoResource {
     @Autowired
     private ProdutoRepository repo;
 
+    @Autowired
+    private ProdutoService service;
+
     @PostMapping("/produtos")
     @ApiOperation(value="Salva um produto")
     @ResponseStatus(HttpStatus.CREATED)
@@ -35,20 +40,43 @@ public class ProdutoResource {
     @GetMapping("/produtos/{id}")
     @ApiOperation(value="Busca um produto único")
     public Produto listarPorId(@PathVariable("id") long id){
-        return repo.findById(id);
+        return service.findById(id);
     }
 
     @DeleteMapping("/produtos/{id}")
     @ApiOperation(value="Deleta um produto")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable("id") long id, Produto produto){
-        produto = repo.findById(id);
-        repo.delete(produto);
+        produto = service.findById(id);
+        service.delete(id, produto);
     }
 
-    @PutMapping("/produtos")
+//    @PutMapping("/produtos")
+//    @ApiOperation(value="Edita um produto")
+//    public Produto editar(@RequestBody Produto produto){
+//        return repo.save(produto);
+//    }
+
+    @PatchMapping("/produtos/{id}")
+    @ApiOperation(value="Edita valor de um produto")
+    public Produto editarValor(@PathVariable("id") long id, @RequestBody Produto produto){
+
+        return service.editarValor(id, produto.getValor());
+    }
+
+//    @PatchMapping("/produtos/{id}")
+//    @ApiOperation(value="Edita o valor através de parâmetro")
+//    public Produto editarValor(@PathVariable("id") long id, @RequestParam("valor") Float valor){
+//        Produto produto = new Produto();
+//        produto = repo.findById(id);
+//        produto.setValor(valor);
+//        return repo.save(produto);
+//    }
+
+    @PutMapping("/produtos/{id}")
     @ApiOperation(value="Edita um produto")
-    public Produto editar(@RequestBody Produto produto){
-        return repo.save(produto);
+    public Produto editar2 (@PathVariable("id") long id, @RequestBody Produto produto){
+        service.atualizar(id, produto);
+        return produto;
     }
 }
